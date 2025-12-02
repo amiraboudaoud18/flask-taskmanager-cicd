53cc6af911ae56936511a75c86bca923f72ec4c8
amiraboudaoud18 <amiraboudaoud30@gmail.com>
2025-12-02 21:02:14 +0100

    Test CI/CD on dev branch - validate the first case of the  workflow

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Version: 1.0.1 - scenario : Testing CI/CD pipeline on dev branch 
+# Version: 1.0.1 - scenario : Testing CI/CD pipeline on dev branch - validating linting and tests 
 
 import os
 from datetime import datetime, date
